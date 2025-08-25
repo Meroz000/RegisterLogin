d89a889b81d37a43959a3ff8ce5e3fb243728109
Meroz000 <samueldebrytosanto12@gmail.com>
2025-08-25 13:29:09 -0300

    Primeiro commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
+import { LoginComponent } from './login-component/login-component';
+import { RegisterComponent } from './register-component/register-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent, children:[{path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    { path: '**', redirectTo: '' }
+]},
+]
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  template: `<router-outlet></router-outlet>`,
+  standalone: true,
+  imports: [RouterOutlet]
 })
-export class App {
-  protected readonly title = signal('LoginRegister');
-}
+export class AppComponent {}
